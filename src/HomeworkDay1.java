edfa5b6d11db83a17b94f89cba7f79eca52485e1
Wookyk <wookiekim888@hotmail.com>
2016-02-09 18:38:24 +0900

    hw

---
+++ src/HomeworkDay1.java
@@ -0,0 +1,40 @@
+
+//public class HomeworkDay1 
+//{
+	//public static void main(String[] args) 
+	//{
+		//System.out.println("Hello, World!");
+	//}
+//}
+
+public class HomeworkDay1
+{
+	public static void main(String[] aug)
+	{
+		System.out.println("+-----+");
+		System.out.println("|Wooky|");
+		System.out.println("+-----+\n");
+		
+		System.out.println("*   *	 **   ****   ****   *	*");
+		System.out.println("*   *   *  *  *  *   *  *    * *");
+		System.out.println("*****  *    * ****   ****     *");
+		System.out.println("*   *  ****** *      *        *");
+		System.out.println("*   *  *    * *      *        *\n");
+		System.out.printf("%d is %s", 16, "anonymous\n");
+		
+		System.out.println("  /////");
+		System.out.println(" | o o |");
+		System.out.println("(|  ^  |)");
+		System.out.println(" | [_] |");
+		System.out.println("  -----");
+		
+		System.out.println("              ______");
+		System.out.println("   ______    |Helloo|");
+		System.out.println("  | o  O |   |Junior|");
+		System.out.println("  |______| < |Coder!|");
+		System.out.println("   |    |     ------");
+		System.out.println("|#+      +#|");
+		System.out.println("  |______|");
+	}
+}
+
