a6afa36ed6d2265d599c50edc6fdf6c07bb10fa7
Wookyk <wookiekim888@hotmail.com>
2016-02-09 18:49:28 +0900

    hw

--- src/homework/day1/HomeworkDay1.java
+++ src/homework/day1/HomeworkDay1.java
@@ -1,12 +1,4 @@
-
-//public class HomeworkDay1 
-//{
-	//public static void main(String[] args) 
-	//{
-		//System.out.println("Hello, World!");
-	//}
-//}
-
+package homework.day1;
 public class HomeworkDay1
 {
 	public static void main(String[] aug)
@@ -14,20 +6,20 @@ public class HomeworkDay1
 		System.out.println("+-----+");
 		System.out.println("|Wooky|");
 		System.out.println("+-----+\n");
-		
+
 		System.out.println("*   *	 **   ****   ****   *	*");
 		System.out.println("*   *   *  *  *  *   *  *    * *");
 		System.out.println("*****  *    * ****   ****     *");
 		System.out.println("*   *  ****** *      *        *");
 		System.out.println("*   *  *    * *      *        *\n");
 		System.out.printf("%d is %s", 16, "anonymous\n");
-		
+
 		System.out.println("  /////");
 		System.out.println(" | o o |");
 		System.out.println("(|  ^  |)");
 		System.out.println(" | [_] |");
 		System.out.println("  -----");
-		
+
 		System.out.println("              ______");
 		System.out.println("   ______    |Helloo|");
 		System.out.println("  | o  O |   |Junior|");
---
+++ src/homework/day1/HomeworkDay1_part2.java
@@ -0,0 +1,7 @@
+package homework.day1;
+public class HomeworkDay1_part2 {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
+
